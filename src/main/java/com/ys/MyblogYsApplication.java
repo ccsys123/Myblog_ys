e37ea418ce15ab842878fb3b4d9f1632a4261ecd
ccsys123 <2161141050@qq.com>
2022-12-05 10:58:38 +0800

    create project

---
+++ src/main/java/com/ys/MyblogYsApplication.java
@@ -0,0 +1,13 @@
+package com.ys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogYsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogYsApplication.class, args);
+    }
+
+}
